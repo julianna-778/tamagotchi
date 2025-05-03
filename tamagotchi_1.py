1d73425f94b1df415e1ff5176a0b37694713d434
julianna-778 <j.gonzalez4@ufl.edu>
2025-05-02 21:53:17 -0400

    Added worried input and submit/clear button

--- tamagotchi_1.py
+++ tamagotchi_1.py
@@ -1,1 +1,56 @@
-print("hello world")
+#git add .
+#git commit -m "Your message"
+#git pushprint("hello world")
+import tkinter as tk
+
+root = tk.Tk()
+root.title("tamagotchi game")
+root.geometry("500x400")
+root.config(bg="#b4dc87") # Change background color to red using hex code
+
+# function: button output
+def out_button_1():
+    rxn_1 = tk.Label(root, text="thank you for telling me .⋅˚ <3 ₊‧ 🜲 ‧₊˚",
+                      font=("Cascadia Code", 10, "bold"), fg="#f2fcdc", bg="#b4dc87")
+    rxn_1.pack()
+    e.delete(0, tk.END)
+
+#creating label widget
+label_1 = tk.Label(root, text="⋆｡‧˚ʚ🍓ɞ˚‧｡⋆Hi Julianna!⋆｡‧˚ʚ🍓ɞ˚‧｡⋆", 
+                   font=("Cascadia Code", 15, "bold"), fg="#f2fcdc", bg="#b4dc87")
+#label_2 = tk.Label(root, text="₊✩‧₊˚౨ৎ˚₊✩‧₊₊✩‧₊˚౨ৎ˚₊✩‧₊₊✩‧₊˚౨ৎ˚₊✩‧₊₊✩‧₊˚౨ৎ˚₊✩‧₊₊✩‧₊˚౨ৎ˚₊✩‧₊₊✩‧₊˚౨ৎ˚₊✩‧₊₊✩‧₊˚౨ৎ˚₊✩‧₊₊✩‧₊˚౨ৎ˚₊✩‧₊₊✩‧₊˚౨ৎ˚₊✩‧₊₊✩‧₊˚౨ৎ˚₊✩‧₊", font=("Cascadia Code", 10), fg="#e4f8ba", bg="#b4dc87")
+label_3 = tk.Label(root, text="What are you worried about?", 
+                   font=("Cascadia Code", 10, "bold" ), fg="#f2fcdc", bg="#b4dc87")
+
+#creating buttons
+button_1 = tk.Button(root, text="Click Me!", font =("Cascadia Code", 10, "bold"), borderwidth = 3, 
+                     padx= 20, command = out_button_1, activebackground ="#b4dc87", activeforeground= "#e9f7cb", fg = "#f2fcdc", bg="#b4dc87")  
+
+#initating input box
+e = tk.Entry(root, width = 50, font =("Cascadia Code", 10, "bold"), borderwidth = 3, bg="#ffd0de", fg = "white")
+e.config(insertbackground="white")
+
+#adding labels to screen
+label_1.pack(pady=20)
+#label_2.pack(pady=5)
+label_3.pack(pady=20)
+#label_2.grid(row=1, column=5)
+
+#adding input box to screen
+e.pack(pady=20)
+
+#adding buttons to screen
+button_1.pack(pady=10)
+
+#wrap text test
+
+
+
+
+
+
+#end
+root.mainloop()
+
+
+
