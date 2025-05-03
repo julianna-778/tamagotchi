63629fee6314708f1b0dddb3f4fb7111d6fa3411
julianna-778 <j.gonzalez4@ufl.edu>
2025-05-02 22:59:12 -0400

    Added character

--- tamagotchi_1.py
+++ tamagotchi_1.py
@@ -1,10 +1,13 @@
 #git add .
 #git commit -m "Your message"
 #git pushprint("hello world")
+from PIL import Image, ImageTk
 import tkinter as tk
+#   import customtkinter as ctk
 
 root = tk.Tk()
 root.title("tamagotchi game")
+root.iconbitmap("C:/Users/julia/OneDrive - University of Florida/Comp Sim/tamagotchi/boba_bear_icon.ico")
 root.geometry("500x400")
 root.config(bg="#b4dc87") # Change background color to red using hex code
 
@@ -18,9 +21,11 @@ def out_button_1():
 #creating label widget
 label_1 = tk.Label(root, text="⋆｡‧˚ʚ🍓ɞ˚‧｡⋆Hi Julianna!⋆｡‧˚ʚ🍓ɞ˚‧｡⋆", 
                    font=("Cascadia Code", 15, "bold"), fg="#f2fcdc", bg="#b4dc87")
-#label_2 = tk.Label(root, text="₊✩‧₊˚౨ৎ˚₊✩‧₊₊✩‧₊˚౨ৎ˚₊✩‧₊₊✩‧₊˚౨ৎ˚₊✩‧₊₊✩‧₊˚౨ৎ˚₊✩‧₊₊✩‧₊˚౨ৎ˚₊✩‧₊₊✩‧₊˚౨ৎ˚₊✩‧₊₊✩‧₊˚౨ৎ˚₊✩‧₊₊✩‧₊˚౨ৎ˚₊✩‧₊₊✩‧₊˚౨ৎ˚₊✩‧₊₊✩‧₊˚౨ৎ˚₊✩‧₊", font=("Cascadia Code", 10), fg="#e4f8ba", bg="#b4dc87")
+image_1 = Image.open("C:/Users/julia/OneDrive - University of Florida/Comp Sim/tamagotchi/poc_body_1.png")
+photo = ImageTk.PhotoImage(image_1.resize((67, 88)))
+label_2 = tk.Label(root, image=photo, bg = "#b4dc87")
 label_3 = tk.Label(root, text="What are you worried about?", 
-                   font=("Cascadia Code", 10, "bold" ), fg="#f2fcdc", bg="#b4dc87")
+                   font=("Cascadia Code",   10, "bold" ), fg="#f2fcdc", bg="#b4dc87")
 
 #creating buttons
 button_1 = tk.Button(root, text="Click Me!", font =("Cascadia Code", 10, "bold"), borderwidth = 3, 
@@ -32,7 +37,7 @@ e.config(insertbackground="white")
 
 #adding labels to screen
 label_1.pack(pady=20)
-#label_2.pack(pady=5)
+label_2.pack(pady=5)
 label_3.pack(pady=20)
 #label_2.grid(row=1, column=5)
 
